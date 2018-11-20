42e27e05e7972f05f7771fc32c8c435f9e3a530a
Roman Kaplan <sromanka@eed.ef.technion.ac.il>
2018-11-20 13:05:14 +0200

    Finished working Aegis implementation

--- AegisSimulation.m
+++ AegisSimulation.m
@@ -10,19 +10,9 @@ PAGES_NUM = 100; %1000;
 BIT_MEAN_WRITES = 1e8;
 BIT_VAR_WRITES = 0.25 * BIT_MEAN_WRITES;
 
-IS_RIDER_USED = true;
-
-if IS_RIDER_USED
-    % ECP Parameters 
-    ECP_MAX_ERRORS_CORRECTED = 4;
-else    % Zombie
-    % ECP Parameters 
-    ECP_MAX_ERRORS_CORRECTED = 6;
-end    
-
+% Aegis
 AEGIS_DIM_A = 9;
-AEGIS_DIM_B = 31;
-
+AEGIS_DIM_B = 61;
 Aegis = AegisMetadata(BIT_MEAN_WRITES, BIT_VAR_WRITES, PAGE_BYTES, BLOCK_BYTES, PAGES_NUM, AEGIS_DIM_A, AEGIS_DIM_B);
 
 %writes
@@ -30,16 +20,14 @@ WRITES_START = 0;
 MAX_WRITES = 10e8; 
 WRITES_RESOLUTION = 100; 
 WRITES_DELTA = (MAX_WRITES-WRITES_START)/WRITES_RESOLUTION;
-WRITES_STEP = 1e6;
+WRITES_STEP = 1e5;
 active_pages_vs_writes_num = zeros(1, 1);
 writes_num_vs_iteration = zeros(1, 1);
 WRITE_WIDTH = BLOCK_BITS;
 
 % Additional insights
-active_not_primary_blocks = zeros(1, 1);
-active_primary_blocks = zeros(1, 1);
-not_active_spare_blocks = zeros(1, 1);
-not_active_dead_blocks = zeros(1, 1);
+faults_num_vs_iteration = zeros(1,1);
+average_faults_per_row = 0;
 
 % perform "virtual" writes
 iter_counter=1;
@@ -48,10 +36,8 @@ writes_performed = WRITES_START;
 IS_SIMULATION_SKIPPED = false; %Load stored .mat files
 while ~Aegis.Memory.isMemoryDead() && ~IS_SIMULATION_SKIPPED
     % iterate over all active pages
-    
-    
     Aegis.writeToRandomRows(WRITES_STEP, WRITE_WIDTH);
-    active_rows_list = Zombie.getActiveRowsList();
+    active_rows_list = Aegis.Memory.getActiveRowsList();
     num_of_active_pages = length(active_rows_list)/PAGE_ROWS;
     
     if mod(writes_performed, 1e10) == 0
@@ -61,13 +47,8 @@ while ~Aegis.Memory.isMemoryDead() && ~IS_SIMULATION_SKIPPED
     %if mod(iter_counter,10) == 0
         active_pages_vs_writes_num(iter_counter) = num_of_active_pages;
         writes_num_vs_iteration(iter_counter) = writes_performed;
-        
-        active_primary_blocks(iter_counter) = length(find(Zombie.block_pairing_table))/PAGE_ROWS;
-        active_not_primary_blocks(iter_counter) = active_pages_vs_writes_num(iter_counter) - active_primary_blocks(iter_counter);
-        
-        not_active_spare_blocks(iter_counter) = (length(find(Zombie.block_pairing_table)) + length(Zombie.spare_blocks_queue))/Zombie.PAGE_ROWS;
-        not_active_dead_blocks(iter_counter) = PAGES_NUM - (active_pages_vs_writes_num(iter_counter) + not_active_spare_blocks(iter_counter));
-        
+        faults_num_vs_iteration(iter_counter) = mean(nonzeros(Aegis.faults_in_killer_rows(:)));
+               
         iter_counter = iter_counter+1;
     %end
     
@@ -97,21 +78,29 @@ figure(66)
     plot(xx_zombie,survmp_zombie,'b');
     
     hold on
-    survmp_active_blocks_not_primary = 100*active_not_primary_blocks/PAGES_NUM;
-    plot(xx_zombie,survmp_active_blocks_not_primary,'green');
     
-    survmp_active_blocks_primary = 100*active_primary_blocks/PAGES_NUM;
-    plot(xx_zombie,survmp_active_blocks_primary, 'magenta');
-    
-    survmp_not_active_spare_blocks = 100*not_active_spare_blocks/PAGES_NUM;
-    plot(xx_zombie,survmp_not_active_spare_blocks,'cyan');
-    
-    survmp_not_active_dead_blocks = 100*not_active_dead_blocks/PAGES_NUM;
-    plot(xx_zombie,survmp_not_active_dead_blocks, 'black');
-    
-    legend('Active Pages: All', 'Active: NOT Primary', 'Active: Primary', 'NOT Active: Spare', 'NOT Active: Dead','Location','northwest');
+    legend('Active Pages: All', 'Location','northwest');
     hold off
 
     xlabel('Average writes/page (B), \sigma=25%')
     ylabel('Percentage of surviving rows')
+    
+figure(67)
+    set(gca, 'FontName', 'Helvetica')
+    set(gca,'FontSize',16,'FontUnits','points');
+    afFigureBackgroundColor = [1, 1, 1];
+    set(gcf, 'color', afFigureBackgroundColor);
+    set(gcf, 'InvertHardCopy', 'off');   
+    set(findall(gca, 'Type', 'Line'),'LineWidth',3);
+
+    % - normalize axis
+    plot(xx_zombie,faults_num_vs_iteration,'r');
+
+    hold on
+
+    legend('Average faults', 'Location','northwest');
+    hold off
+
+    xlabel('Average writes/page (B), \sigma=25%')
+    ylabel('Average number of faults in all blocks')
 
